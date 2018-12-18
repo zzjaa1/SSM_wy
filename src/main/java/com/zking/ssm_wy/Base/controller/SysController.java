ed1cfdbb5be8581c2e02b24280e2856c67813041
zzj <997263576@qq.com>
2018-12-18 15:12:53 +0800

    11

--- src/main/java/com/zking/ssm_wy/Base/controller/SysController.java
+++ src/main/java/com/zking/ssm_wy/Base/controller/SysController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
